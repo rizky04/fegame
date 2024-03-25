058cfdaeb8087927ca74c88645c42fda72d9c107
rizky04 <carlivanhowten@gmail.com>
2024-03-25 16:21:27 +0700

    cobaaaa

--- components/organisme/SideBar/Profile.tsx
+++ components/organisme/SideBar/Profile.tsx
@@ -1,13 +1,36 @@
-import Image from 'next/image'
-import React from 'react'
+import { JWTPayloadTypes, UserTypes } from '@/services/data-types';
+import Cookies from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
+import Link from "next/link";
 
 export default function Profile() {
+  const [user, setUser] = useState({
+    avatar: '',
+    name: '',
+    email: '',
+    id: '',
+  });
+  const API_IMG = process.env.NEXT_PUBLIC_IMG;
+  useEffect(()=>{
+    const token = Cookies.get('token');
+    if (token) {
+      const jwtToken = atob(token);
+      const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+      const userFormPayload: UserTypes = payload.player; 
+      
+      console.log(user.avatar);
+      setUser(userFormPayload);    
+    }
+  },[]);
   return (
     <>
      <div className="user text-center pb-50 pe-30">
-        <Image src="/img/avatar-1.png" width="90" height="90" className="img-fluid mb-20" alt='avatar' />
-        <h2 className="fw-bold text-xl color-palette-1 m-0">Shayna Anne</h2>
-        <p className="color-palette-2 m-0">shayna@anne.com</p>
+        <Image src={`${API_IMG}/${user.avatar}`}  width="90" height="90" className="img-fluid mb-20" alt="avatar" />
+        <h2 className="fw-bold text-xl color-palette-1 m-0">{user.name}</h2>
+        <p className="color-palette-2 m-0">{user.email}</p>
+        <p className="color-palette-2 m-0">{user.id}</p>
     </div>
     </>
   )
